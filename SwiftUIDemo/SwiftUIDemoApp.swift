f7a516a0f9836c78f1988f5f119e1cd6c900b90c
Nagaraj V Rao <achuthavrao@gmail.com>
2024-07-29 12:40:13 +0530

    Initial Commit

---
+++ SwiftUIDemo/SwiftUIDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIDemoApp.swift
+//  SwiftUIDemo
+//
+//  Created by Nagaraj V Rao on 29/07/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
